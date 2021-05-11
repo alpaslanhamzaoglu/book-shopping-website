a3bc41cd1f78152745ddf2af0df0892418599883
alpaslanhamzaoglu <44805408+alpaslanahamzaoglu@users.noreply.github.com>
2021-05-11 22:50:02 +0300

    first codes

---
+++ admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <table class="table">
+            <thead>
+                <th scope=“col”>asdfhjgsadhfjas</th>
+                <th scope=“col”>erg</th>
+                <th scope=“col”>wrhh</th>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope=“row”> asdfa </th>
+                    <td>adfsadghfjgadsfhjdsa</td>
+                    <td>adfsadga</td>                    
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</html>
