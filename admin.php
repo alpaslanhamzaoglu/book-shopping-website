9d2be7a788f05f6e6cc7a3c65ac8d436162dbfaf
s21alperenyildiz <alperenyildiz@sabanciuniv.edu>
2021-05-12 23:00:13 +0300

    standart user için ekleme özelliği

--- admin.php
+++ admin.php
@@ -11,20 +11,77 @@
     <div class="container">
         <table class="table">
             <thead>
-                <th scope=“col”>alperen</th>
-                <th scope=“col”>erg</th>
-                <th scope=“col”>wrhh</th>
+                <th scope=“col”>id</th>
+                <th scope=“col”>name</th>
+                <th scope=“col”>surname</th>
+                <th scope=“col”>email</th>
+                <th scope=“col”>admin / mod since</th>
+                <th scope=“col”>password</th>
             </thead>
             <tbody>
-                <tr>
-                    <th scope=“row”> asdfa </th>
-                    <td>alpaslan</td>
-                    <td>adfsadga</td>                    
-                </tr>
+                    <?php 
+                    
+                        include "config.php";
+                        $sql_statement = "SELECT * FROM users";
+                        $result = mysqli_query($db, $sql_statement);
+                        while($row = mysqli_fetch_assoc($result))
+                        {
+                            $id = $row['uID'];
+                            $email = $row['email'];
+                            $name = $row['name'];
+                            $surname = $row['surname'];
+                            $password = $row['upassword'];
+                            $since = "not an admin / mod";
+
+                            echo "<tr>";
+                            echo "<th scope=“row”> $id </th>";
+                            echo "<td> $name </td>";
+                            echo "<td> $surname </td>";
+                            echo "<td> $email </td>";
+                            echo "<td> $since </td>";
+                            echo "<td> $password </td>";
+                            echo "<tr/>";
+
+                        }
+                    
+                    ?>                  
             </tbody>
+            
         </table>
+        <div class="dropdown">
+        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+        <span class="caret"></span></button>
+        <ul class="dropdown-menu">
+            <li><a href="#">HTML</a></li>
+            <li><a href="#">CSS</a></li>
+            <li><a href="#">JavaScript</a></li>
+        </ul>
+        <button class="btn btn-danger">delete</button>
+        </div>
+        <br>
+    <form action="createUser.php" method="POST">
+    <div class="form-group">
+
+        <label for="uID">uID</label>
+        <input type="id" class="form-control" name="uID" placeholder="id">
+
+        <label for="userName">name</label>
+        <input class="form-control" name="userName" placeholder="name">
+
+        <label for="userSurname">surname</label>
+        <input class="form-control" name="userSurname" placeholder="surname">
+
+        <label for="userEmail">email</label>
+        <input type="email" class="form-control" id="userEmail" name="userEmail" placeholder="Email">
+
+        <label for="password">password</label>
+        <input class="form-control" id="password" name="password" placeholder="password">
+    </div>
+    <button type="submit" class="btn btn-primary">ekle</button>
+    <button type="submit" class="btn btn-secondary">Search</button>
+    </form>
+
     </div>
 </body>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-</html>
